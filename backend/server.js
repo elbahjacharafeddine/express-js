4d69e5856b17e562b577dac84e6092abee8d3cd3
elbahjacharafeddine <charafensaj@gmail.com>
2023-07-10 19:21:38 +0100

    elbahja commit

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,8 @@ import { notFound, errorHandler } from './middleware/errorMiddleware.js';
 import userRoutes from './routes/userRoutes.js';
 import User from './models/userModel.js';
 
+import cors from 'cors'
+
 const port = process.env.PORT || 5000;
 
 connectDB();
@@ -16,7 +18,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cors({ origin: 'http://localhost:3000' }));
 app.use(cookieParser());
 
 app.use('/api/users', userRoutes);
